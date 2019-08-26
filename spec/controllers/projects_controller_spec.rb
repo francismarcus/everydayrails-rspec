8380f33195303de83be259e4d92e47edb9b14b93
francismarcus <marcusmagnusson@outlook.com>
2019-08-26 09:31:29 +0200

    update projects controller

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -2,14 +2,33 @@ require "rails_helper"
 
 RSpec.describe ProjectsController, type: :controller do
   describe "#index" do
-    before do
-      @user = FactoryBot.create(:user)
+    context "as an authenticated user" do
+      before do
+        @user = FactoryBot.create(:user)
+        @project = FactoryBot.create(:project, owner: @user)
+      end
+
+      it "adds a project" do
+        project_params = FactoryBot.attributes_for(:project)
+        sign_in @user
+        expect {
+          post :create, params: { project: project_params }
+        }.to change(@user.projects, :count).by(1)
+      end
+
+      it "reponds" do
+        sign_in @user
+        get :show, params: { id: @project.id }
+        get :index
+        expect(response).to be_success
+      end
     end
 
-    it "reponds" do
-      sign_in @user
-      get :index
-      expect(response).to be_success
+    context "while not authenticated" do
+      it "redirects to sign in" do
+        get :index
+        expect(response).to redirect_to "/users/sign_in"
+      end
     end
   end
 end
